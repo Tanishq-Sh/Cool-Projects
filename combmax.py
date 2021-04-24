23b7160e536c9032edc9c456d37c0cdd1db59ca3
Tanishq Sharma <65755600+Tanishq-Sh@users.noreply.github.com>
2021-04-24 14:12:04 +0530

    Create combmax.py
    Finding max occurring combinations

---
+++ combmax.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import itertools
+import re
+
+df = pd.read_csv('data.csv')
+dfk = pd.read_csv('key.csv')
+
+# print(df)
+
+# # print(dfk)
+# print(list(itertools.combinations(dfk['Issue'], 3)))
+
+comb = list(itertools.combinations(dfk['Issue'], 2))
+
+counter = 0
+arr = []
+rep = 0
+maxcoDesc = str()
+maxcoRep = 0
+for x in comb:
+    for item in df['Desc']:
+        for y in x:
+            if re.search(y, item) is not None:
+                counter = counter + 1
+                # print(re.search(y, item))
+                # print(str(len(x)) + ' = ' + str(counter))
+            else:
+                counter = 0
+                break
+        if counter == len(x):
+            # print('True')
+            rep = rep + 1
+        counter = 0
+    if rep > maxcoRep:
+        maxcoRep = rep
+        maxcoDesc = str(x)
+
+    # print(str(x) + ' : ' + str(rep))
+    rep = 0
+print(maxcoDesc + ' : ' + str(maxcoRep))
